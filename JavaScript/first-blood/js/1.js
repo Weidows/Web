0042eaa952214ec1ec757db030b6f6b1fabf2b6c
Weidows <utsuko27@qq.com>
2021-07-28 11:07:40 +0800

    🎃 boom!

---
+++ JavaScript/first-blood/js/1.js
@@ -0,0 +1,13 @@
+/*
+ * @?: *********************************************************************
+ * @Author: Weidows
+ * @Date: 2020-12-20 22:47:20
+ * @LastEditors: Weidows
+ * @LastEditTime: 2021-01-07 16:58:49
+ * @FilePath: \Weidows\Web\JavaScript\first-blood\js\1.js
+ * @Description:
+ * @!: *********************************************************************
+ */
+"use strict";
+// alert只能用在浏览器中,在命令行/node中无效.
+// alert("HelloWorld-2");
